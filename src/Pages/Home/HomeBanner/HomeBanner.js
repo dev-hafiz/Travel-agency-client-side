7157687fdc841acddca62392d4a657123cfda3d2
okshi <johnstarhafiz@gmail.com>
2022-01-25 21:55:42 +0600

    Finished initial setup for carosuel in the top

--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -5,7 +5,8 @@ import s3 from '../../../../src/TravelCarosuel/slider3.jpg';
 
 const HomeBanner = () => {
   return (
-    <div>
+    <div> 
+    {/* Top carosel  */}
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
